f270d0431f0d1f487c5aadf2a41f54713d95692b
Juan Quiroga <jquiroga1991@gmail.com>
2019-05-01 20:56:01 -0300

    heroku changes for deploy
    Signed-off-by: Juan Quiroga <jquiroga1991@gmail.com>

--- mandala/settings.py
+++ mandala/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7%sy8c)0m4#z13abo39(yac9=1)fz&9#_j^2p%gcs*yn6p&ez#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mandalaconsultingapp.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,6 +133,11 @@ STATICFILES_DIRS = [
 #STATIC_DIRS,
 
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL="/media/"
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
